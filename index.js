2cfb17669fb036b370abd368fef0838f5ae6a970
Your Name <you@example.com>
2022-04-11 21:31:09 +0200

    lagt till add booking

--- index.js
+++ index.js
@@ -15,6 +15,20 @@ app.get("/getBookings", async (req, res) => {
     }
 })
 
+app.post("/addBooking", async (req, res) => {
+    try {
+        const { bodyPart } = req.body;
+        const newBooking = await pool.query(
+            "INSERT INTO bookings (bodyPart) VALUES ($1) RETURNING *", 
+            [bodyPart]
+        );
+
+        res.json(newBooking);
+    } catch (error) {
+        console.error(error.message)
+    }
+})
+
 
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
