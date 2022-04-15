9e0b518f087ee4b712baee441a4bce62f8450d77
Your Name <you@example.com>
2022-04-15 11:18:54 +0200

    försöker fixa applicaton error i heroku

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const pool = require("./database");
-const bodyParser = require('body-parser')
 const port = process.env.PORT || 3000;
 
 app.use(cors());
